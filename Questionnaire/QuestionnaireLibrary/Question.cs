8e5964a223f8f30046382861ee8847c5108555ae
Lynn Delaere <lynn.delaere@hotmail.be>
2024-05-14 21:13:26 +0200

    Update

---
+++ Questionnaire/QuestionnaireLibrary/Question.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionnaireLibrary
+{
+    public class Question
+    {
+        // Attributes
+        private string text;
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+
+        }
+        public List<Answer> PossibleAnswers = new List<Answer>();
+        private string imageURL;
+        public string ImageURL
+        {
+            get { return imageURL; }
+            set { imageURL = value; }
+        }
+        // Constructor
+        public Question(string text)
+        {
+            this.text = text;
+        }
+        // Methods
+        public void Add(Answer answer)
+        {
+            PossibleAnswers.Add(answer);
+        }
+        public Answer GetAnswer(int index)
+        {
+            return PossibleAnswers[index];
+        }
+        public string CorrectAnswer()
+        {
+            foreach (Answer answer in PossibleAnswers)
+            {
+                if (answer.IsCorrect)
+                {
+                    return answer.Text;
+                }
+            }
+            return null;
+        }
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
